ebcc5a653720b88017305ff2ebade28806bc0e42
Harley Glover-Newson <harleygn@gmail.com>
2017-07-06 20:10:05 +0100

    Added a power LED and temperature reading LED
    When powered on, a (red) LED is also powered on to indicate this, and in the future this will be integrated into the unit's housing as the main board will not be visible.
    
    Another (green) LED has been added which lights only while the temperature is being requested.

--- DS18B20_Temp.ino
+++ DS18B20_Temp.ino
@@ -2,26 +2,29 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
+#define ONE_WIRE_BUS 2 // Data wire is connected to pin 2
+
+int powerLED = 3; // LED indicating power is connected to pin 3
+int statusLED = 4; // LED indicating temperature reading is connected to pin 4
+OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature. 
 
 void setup(void)
 {
+  pinMode(statusLED, OUTPUT); // Setup status LED
+  pinMode(powerLED, OUTPUT); // Setup power LED
   Serial.begin(9600); //Begin serial communication
+  digitalWrite(powerLED, HIGH); // Indicates the board is powered
   Serial.println("Arduino Digital Temperature"); //Print a message
   sensors.begin();
 }
 
 void loop(void)
 { 
-  // Send the command to get temperatures
-  sensors.requestTemperatures();  
+  digitalWrite(statusLED, HIGH); // Indicates that temperature is being read
+  sensors.requestTemperatures(); // Send the command to get temperatures
   Serial.print("Temperature is: ");
   Serial.println(sensors.getTempCByIndex(0)); // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  //Update value every 1 sec.
-  delay(1000);
+  digitalWrite(statusLED, LOW); // Indicates that temperature is no longer being read
+  delay(15000); //Update value every 1 sec.
 }

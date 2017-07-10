1bfd1c992cbb36e03a0502c625141feacb25c80c
Harley Glover-Newson <harleygn@gmail.com>
2017-07-10 16:21:36 +0100

    Only prints the temperature value
    Removed the unnecessary printing before and the temperature value

--- DS18B20_Temp.ino
+++ DS18B20_Temp.ino
@@ -15,7 +15,6 @@ void setup(void)
   pinMode(powerLED, OUTPUT); // Setup power LED
   Serial.begin(9600); //Begin serial communication
   digitalWrite(powerLED, HIGH); // Indicates the board is powered
-  Serial.println("Arduino Digital Temperature"); //Print a message
   sensors.begin();
 }
 
@@ -23,7 +22,6 @@ void loop(void)
 { 
   digitalWrite(statusLED, HIGH); // Indicates that temperature is being read
   sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.print("Temperature is: ");
   Serial.println(sensors.getTempCByIndex(0)); // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
   digitalWrite(statusLED, LOW); // Indicates that temperature is no longer being read
   delay(15000); //Update value every 1 sec.

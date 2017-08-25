b434135bf23d390fdb9fa11d8613a89d9158f05f
harleygn <harleygn@gmail.com>
2017-08-25 13:48:36 +0000

    Initial development of recording values

--- arduino_serial.py
+++ arduino_serial.py
@@ -1,7 +1,13 @@
 import serial
+
+def createBlankCsv(date):
+	path = date + "temp_reading.csv"
+	with open(path, "a+") as currentCsv:
+		print("key, date, time, temp\n")
+	
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
 	temp = ser.readline()
-	temp = temp.rstrip()
+	temp = temp.decode().strip()
 	print(temp)

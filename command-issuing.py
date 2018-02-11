ea9fe741bb7d4198c0a281789a0fe6e65aecebdf
Harley Glover-Newson <harleygn@gmail.com>
2018-02-11 14:47:05 +0000

    Checks whether the schedule should be active

--- command-issuing.py
+++ command-issuing.py
@@ -6,11 +6,22 @@ def read_schedule(schedule_date):
     schedule_path = 'schedules/' + schedule_date + '_schedule.json'
     with open(schedule_path, 'r') as schedule_json:
         schedule = json.load(schedule_json)
-    morning_schedule = schedule['schedule'][0]
-    evening_schedule = schedule['schedule'][1]
-    return morning_schedule, evening_schedule
+    return schedule['schedule']
+
+
+def check_timings(schedule):
+    time = datetime.datetime.now()
+    hour = time.hour
+    morning_start = datetime.datetime.strptime(schedule[0]['start'], '%H:%M:%S')
+    morning_end = datetime.datetime.strptime(schedule[0]['end'], '%H:%M:%S')
+    afternoon_start = datetime.datetime.strptime(schedule[1]['start'], '%H:%M:%S')
+    afternoon_end = datetime.datetime.strptime(schedule[1]['end'], '%H:%M:%S')
+    if hour < 12:
+        return morning_start <= time < morning_end
+    elif hour >= 12:
+        return afternoon_start <= time < afternoon_end
 
 
 if __name__ == '__main__':
     date = date = datetime.datetime.now().strftime("%d-%m-%Y")
-    print(read_schedule(date))
+    print(check_timings(read_schedule(date)))
